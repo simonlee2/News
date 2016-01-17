dfe3395db781df0375f273c37c4f56f874dbf749
Simon Lee <simon911011@gmail.com>
2016-01-17 16:24:45 -0600

    Add iOS and OSX example projects to workspace

---
+++ FeedKit-OSX/FeedKit-OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FeedKit-OSX
+//
+//  Created by Shao-Ping Lee on 1/17/16.
+//  Copyright © 2016 Simon Lee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ FeedKit-iOS/FeedKit-iOS/FeedKit-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedKit-iOS
+//
+//  Created by Shao-Ping Lee on 1/17/16.
+//  Copyright © 2016 Simon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
